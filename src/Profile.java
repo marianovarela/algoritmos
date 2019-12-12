236d1cb64937f04983a323ed40a04776d1d5d05d
Mariano Varela <marianogvarela@gmail.com>
2019-12-12 20:33:36 -0300

    se borra codigo sin usar

--- src/Profile.java
+++ src/Profile.java
@@ -60,85 +60,8 @@ public class Profile {
 	    // the respective strings 
 	    int xans[] = new int[l + 1];  
 	    int yans[] = new int[l + 1]; 
-	    //aca hay que armar el alineamiento
-	    /*while ( !(i == 0 || j == 0)) 
-	    { 
-	        if (x.charAt(i - 1) == y.charAt(j - 1)) 
-	        { 
-	            xans[xpos--] = (int)x.charAt(i - 1); 
-	            yans[ypos--] = (int)y.charAt(j - 1); 
-	            i--; j--; 
-	        } 
-	        else if (dp[i - 1][j - 1] + pxy == dp[i][j]) 
-	        { 
-	            xans[xpos--] = (int)x.charAt(i - 1); 
-	            yans[ypos--] = (int)y.charAt(j - 1); 
-	            i--; j--; 
-	        } 
-	        else if (dp[i - 1][j] + pgap == dp[i][j]) 
-	        { 
-	            xans[xpos--] = (int)x.charAt(i - 1); 
-	            yans[ypos--] = (int)'_'; 
-	            i--; 
-	        } 
-	        else if (dp[i][j - 1] + pgap == dp[i][j]) 
-	        { 
-	            xans[xpos--] = (int)'_'; 
-	            yans[ypos--] = (int)y.charAt(j - 1); 
-	            j--; 
-	        } 
-	    } 
-	    while (xpos > 0) 
-	    { 
-	        if (i > 0) xans[xpos--] = (int)x.charAt(--i); 
-	        else xans[xpos--] = (int)'_'; 
-	    } 
-	    while (ypos > 0) 
-	    { 
-	        if (j > 0) yans[ypos--] = (int)y.charAt(--j); 
-	        else yans[ypos--] = (int)'_'; 
-	    } 
-	  
-	    // Since we have assumed the  
-	    // answer to be n+m long,  
-	    // we need to remove the extra  
-	    // gaps in the starting id  
-	    // represents the index from  
-	    // which the arrays xans, 
-	    // yans are useful 
-	    int id = 1; 
-	    for (i = l; i >= 1; i--) 
-	    { 
-	        if ((char)yans[i] == '_' &&  
-	            (char)xans[i] == '_') 
-	        { 
-	            id = i + 1; 
-	            break; 
-	        } 
-	    } 
-	  
-	    // Printing the final answer 
-	    System.out.print("Minimum Penalty in " +  
-	                     "aligning the genes = "); 
-	    System.out.print(dp[m][n] + "\n"); 
-	    System.out.println("The aligned genes are :"); 
-	    String alignmentOne = "";
-	    for (i = id; i <= l; i++) 
-	    { 
-	    	alignmentOne += (char)xans[i];
-	        System.out.print((char)xans[i]); 
-	    } 
-	    System.out.print("\n"); 
-	    String alignmentTwo = "";
-	    for (i = id; i <= l; i++) 
-	    { 
-	    	alignmentTwo += (char)yans[i];
-	    	System.out.print((char)yans[i]); 
-	    } 
-	    */
-//	    return dp[m][n]; 
+	    
 	    printMatrix(dp);
-	    System.out.println("el score final es " + dp[m][n]);
 	    printMatrix(newProfile);
 	    return new ProfileSequence(idx, x,  dp[m][n], newProfile);
 	}
